c80f7adcb94eb2389206a409d0df7597d898a9a8
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-08-06 21:24:41 +0200

    Completed header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-
+import { useState } from "react"
+import headerImg from "./images/bg.png"
 
 function App() {
+  //tailwind styles
+  const container = ``
+  const header = `bg-cover bg-center p-8 text-neutral-300`
+  const title = `text-2xl`
+  const description = ``
+  //
 
   return (
-   <div className="bg-green-500">
-    <h1>app here</h1>
+   <div className={container}>
+    <header className={header} style={{backgroundImage: `url(${headerImg})`,}}>
+      <h1 className={title}>Gyos's diner</h1>
+      <p className={description}>The best burgers and pizzas in town.</p>
+    </header>
+    <main>
+      <h1>dishes here</h1>
+      <div>
+        <p>complete order here</p>
+      </div>
+      <div>
+        <p>form here</p>
+      </div>
+    </main>
    </div>
   )
 }
